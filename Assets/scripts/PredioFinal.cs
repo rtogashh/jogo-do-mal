aba68c3c432da4a3fa1647c7c19f0ae44b306ea5
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2026-04-27 18:25:43 -0300

    Atualização de Natureza
    -Adicionada grama
    -Nova Textura de Chão verde base
    -Novas Árvores
    -Novas Pedras

--- Assets/scripts/PredioFinal.cs
+++ Assets/scripts/PredioFinal.cs
@@ -3,12 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class PredioFinal : MonoBehaviour
 {
-    /*[SerializeField] private string sceneName = "Menu";
-    private void OnCollisionEnter(Collision collision)
+    [SerializeField] private string sceneName = "Menu";
+    void OnTriggerEnter(Collider collision)
     {
+        Debug.Log("Collided with: " + collision.gameObject.name);
         if(collision.CompareTag("Player"))
         {
             SceneManager.LoadScene(sceneName);
         }
-    }*/
+    }
 }
