20d2f91ab00fbb5dfce9fee0b26e9717965fad08
LuanVelho <146845889+LuanVelho@users.noreply.github.com>
2026-04-28 20:00:35 -0300

    Natureza e Assets
    -Adicionado Árvores
    -Adicionado Fogo
    -Adicionado Caminho pelas montanhas
    -Novos Postes de luz
    -Atualização de Assets
    -Atualização da Câmera

--- Assets/scripts/MainMenuManager.cs
+++ Assets/scripts/MainMenuManager.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    private void Awake()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
